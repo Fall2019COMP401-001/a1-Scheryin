763277a5c83bb5df2d5f953283503d7ee738d410
scheryin <scheryin@scheryin-pc>
2019-09-01 00:28:10 -0400

    Test one of submitting assignment.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,31 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		int numberOfItems = scan.nextInt();
+		for (int i = 0; i<numberOfItems; i++) {
+			String[] itemName = new String[numberOfItems];
+			itemName[i] = scan.next();
+			double[] price = new double[numberOfItems];
+			price[i] = scan.nextDouble();
+		}
+		int totalNumberOfCustomers = scan.nextInt();
+		String[] firstName = new String[totalNumberOfCustomers];
+		String[] lastName = new String[totalNumberOfCustomers];
+		int[] itemsBought = new int[totalNumberOfCustomers];
+		int[] quantity = new int[totalNumberOfCustomers];
+		String[] itemName2 = new String[totalNumberOfCustomers];
+		double[] sumsArr = new double[totalNumberOfCustomers];
+		double sum = 0;
+		for (int i = 0; i<totalNumberOfCustomers; i++) {
+			firstName[i] = scan.next();
+			lastName[i] = scan.next();
+			itemsBought[i] = scan.nextInt();
+			sum = 0;
+			for (int k = 0; k<itemsBought[i]; k++) {
+				quantity[k] = scan.nextInt();
+				itemName2[k] = scan.next();
+					}
+				}
 	}
 }
+
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,31 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int totalCustomers = scan.nextInt();
 		
+		String[] customerNames = new String[totalCustomers];
+		String[] customerLastNames = new String[totalCustomers];
+		int[] items = new int[totalCustomers];
+		double[] sumsArr = new double[totalCustomers];
+		double sum = 0;
+		for(int i = 0; i<customerNames.length; i++) {
+			customerNames[i] = scan.next();
+			customerLastNames[i] = scan.next();
+			items[i] = scan.nextInt();
+			sum = 0;
+			for(int k = 0; k<items[i]; k++) {
+				int[] quantity = new int[totalCustomers];
+				quantity[k] = scan.nextInt();
+				String[] itemName = new String[totalCustomers];
+				itemName[k] = scan.next();
+				double[] price = new double[totalCustomers];
+				price[k] = scan.nextDouble();
+				sum = price[k] * quantity[k] + sum;
+			}
+			sumsArr[i] = sum;
+		}
+	for(int i = 0; i<totalCustomers; i++) {
+		System.out.println(customerNames[i].charAt(0) + ". " + customerLastNames[i] + ": " + sumsArr[i]);
+	}
 	}
 }
