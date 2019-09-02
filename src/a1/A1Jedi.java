bbd3a7d9312fd08dae5a661860ff436b479f7138
scheryin <scheryin@scheryin-pc>
2019-09-02 01:15:01 -0400

    Jedi Test

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,28 +9,43 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-	int count = scan.nextInt();
-	String [] itemsInStore = new String[count];
-	double[] price = new double[count];
-	for(int i = 0; i<count; i++) {
-		itemsInStore[i] = scan.next();
-		price[i] = scan.nextDouble();
+				int count = scan.nextInt();
+				String [] itemsInStore = new String[count];
+				double[] price = new double[count];
+				for(int i = 0; i<count; i++) {
+					itemsInStore[i] = scan.next();
+					price[i] = scan.nextDouble();
+				}
+				int numOfCust = scan.nextInt();
+				String[] first = new String[numOfCust];
+				String[] last = new String[numOfCust];
+				int[] howManyCus = new int[itemsInStore.length];
+				int[] quantityBought = new int[itemsInStore.length];
+				for(int i = 0; i<numOfCust; i++) {
+					first[i] = scan.next();
+					last[i] = scan.next();
+					int itemsBought = scan.nextInt();
+					for (int k = 0; k<itemsBought; k++) {
+						int quantity = scan.nextInt();
+						String nameOfItem = scan.next();
+						if(itemsInStore[k].equals(nameOfItem)) {
+						}
+						for (int j = 0; j<itemsInStore.length; j++) {
+						if(itemsInStore[j].equals(nameOfItem)) {
+						quantityBought[j] += quantity;
+						howManyCus[j]++;
+
+						}
+						}
+
+					}
+				}
+				for(int i = 0; i<itemsInStore.length; i++) {
+					if(quantityBought[i] > 0) {
+				System.out.println(howManyCus[i] + " customers " + quantityBought[i] + " " + itemsInStore[i]);
+					} else {
+						System.out.println("No customers bought " + itemsInStore[i]);
+					}
+				}
 	}
-	int numOfCust = scan.nextInt();
-	String[] first = new String[numOfCust];
-	String[] last = new String[numOfCust];
-	double [] sumArr = new double[numOfCust];
-	double sum = 0;
-	for(int i = 0; i<numOfCust; i++) {
-		first[i] = scan.next();
-		last[i] = scan.next();
-		int itemsBought = scan.nextInt();
-		int[] quantity = new int[itemsBought];
-		String[] nameOfItem = new String[itemsBought];
-		for (int k = 0; k<itemsBought; k++) {
-			quantity[k] = scan.nextInt();
-			nameOfItem[k] = scan.next();
-		}
 	}
-	}
-}
